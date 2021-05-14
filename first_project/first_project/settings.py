92f452fec4a050424543e6b806c4cbf0c38faa4b
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-05-14 14:53:39 +0300

    DEBUG: settings. templates

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'first_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
